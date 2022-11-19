07707e27bffdd6a1438c13600f4a17fde2bd9847
Ke Ren <rkglimmer16@gmail.com>
2022-11-18 20:57:53 -0800

    Tilesets for wfc

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,94 @@
 import React from 'react';
 import './App.css';
 
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link as RouterLink } from "react-router-dom";
+
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
 class App extends React.Component {
 
   render() {
+    const pages: {linkName:string, route:string}[] = [
+      {linkName: "Home", route:"/"},
+      {linkName: "Soduku", route:"/sudoku"},
+      {linkName: "2D WFC", route:"/WFC-tile-2d"},
+      {linkName: "raw tile 2D WFC", route:"/WFC-raw-tile-2d"},
+    ];
     return (
       <div className="App">
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/sudoku">sudoku</Link>
-          <Link to="/WFC-tile-2d">2D WFC</Link>
-          <Link to="/WFC-raw-tile-2d">2D WFC Raw Tile</Link>
-        </nav>
+        
+
+      <AppBar position="static">
+            <Container maxWidth="xl">
+              <Toolbar disableGutters>
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={()=>{}}
+                    color="inherit"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'left',
+                    }}
+                    open={false}
+                    onClose={()=>{}}
+                    sx={{
+                      display: { xs: 'block', md: 'none' },
+                    }}
+                  >
+                    {pages.map((page) => (
+                      <MenuItem component={RouterLink} key={page.linkName} to={page.route} onClick={()=>{}}>
+                        <Typography textAlign="center">{page.linkName}</Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                  {pages.map((page) => (
+                    <Button
+                      component={RouterLink}
+                      key={page.linkName}
+                      to={page.route}
+                      sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                      {page.linkName}
+                    </Button>
+                  ))}
+                </Box>
+              </Toolbar>
+            </Container>
+          </AppBar>
 
         <Outlet></Outlet>
       </div>
--- src/pages/2D-raw-tile-page.tsx
+++ src/pages/2D-raw-tile-page.tsx
@@ -4,6 +4,11 @@ import { RawTileContainer } from "../app/WFC/models/rawTileContainer";
 import { RawTile } from "../models/wfc/tile";
 import { MetricDirection2D } from "../utils/enums";
 
+//style
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
 interface WFCRawTilePage2DProps {
     rawTileContainer: RawTileContainer
 }
@@ -44,27 +49,39 @@ export default class WFCTRawTilePage2D extends React.Component<WFCRawTilePage2DP
                 'height': `${imageSize}px`
             };
             const a = <div key={id} style={{display:'flex', flexDirection: 'column', alignItems: 'center', width: "20%"}}>
-                <p>{rawTile.tileId}</p>
+                {/* <p>{rawTile.tileId}</p> */}
                 <img key={id} style={cellStyle} src={image.imageAddress} alt=""/>
                 <p>Image address:</p>
-                <input type="text" value={rawTile.imageAddress} onChange={()=>{}}/>
-                <button onClick={()=>{}}>change</button>
+                <TextField
+                    id="standard-disabled"
+                    type="text"
+                    label="Image source"
+                    variant="outlined"
+                    value={rawTile.imageAddress} onChange={()=>{}}
+                    />
+                <Button variant="contained" onClick={()=>{}}>change</Button>
 
                 <div>
                     <p>connectors:</p>
                     <div>
                             <p>Up</p>
-                            <input type="text" value={rawTile.upConnection} onChange={()=>{}}/>
-                            <button onClick={()=>{}}>change</button>
+                            <TextField
+                                id="standard-disabled"
+                                type="text"
+                                label="Up Connector"
+                                variant="outlined"
+                                value={rawTile.upConnection} onChange={()=>{}}
+                                />
+                            <Button variant="contained" onClick={()=>{}}>change</Button>
                         
                     </div>
                 </div>
-                <button onClick={()=>{
+                <Button variant="contained" onClick={()=>{
                         this.rawTileContainer.setDefaultTileById(rawTile.tileId);
                         this.setState({rawTileContainer: this.rawTileContainer});
                     }}>
                     Set as default
-                </button>
+                </Button>
             </div>;
             allImages.push(a);
         }
@@ -76,33 +93,44 @@ export default class WFCTRawTilePage2D extends React.Component<WFCRawTilePage2DP
                 <div style={{display:'flex', flexDirection:'row'}}>
                     {allImages}
                 </div>
-
-                <form onSubmit={(e)=>{
-                    const id = this.rawTileContainer.addTileByImageAddress(this.state.addRawTile.imageAddress);
-
-                    const tile = this.rawTileContainer.getTileById(id);
-
-                    tile.setConnection(MetricDirection2D.UP, ["A", "B", "A"]);
-
-                    this.setState({rawTileContainer:this.rawTileContainer});
-                    e.preventDefault();
-                }}>
-                    <input type="text" 
+                <Box component="form"
+                    onSubmit={(e:any)=>{
+                        const id = this.rawTileContainer.addTileByImageAddress(this.state.addRawTile.imageAddress);
+    
+                        const tile = this.rawTileContainer.getTileById(id);
+    
+                        tile.setConnection(MetricDirection2D.UP, ["A", "B", "A"]);
+    
+                        this.setState({rawTileContainer:this.rawTileContainer});
+                        e.preventDefault();
+                    }}>
+                    <TextField
+                        id="standard-disabled"
+                        type="text"
+                        label="Tile image address"
+                        variant="outlined"
                         value={this.state.addRawTile.imageAddress} 
-                        onChange={(e)=>{this.setState({addRawTile:{imageAddress:e.target.value}})}} />
-                    <input type="submit" value="Add new tile" />
-                </form>
+                        onChange={(e)=>{this.setState({addRawTile:{imageAddress:e.target.value}})}}
+                        />
+                    <Button variant="contained" type="submit">Add new tile</Button>
+                </Box>
 
-                <input type="text" name="" id="" value={this.state.findConnectableId} 
-                    onChange={(e)=>{this.setState({findConnectableId:e.target.value})}} />
+                <TextField
+                    id="standard-disabled"
+                    type="text"
+                    label="Connectable ID"
+                    variant="outlined"
+                    value={this.state.findConnectableId} 
+                    onChange={(e)=>{this.setState({findConnectableId:e.target.value})}}
+                    />
 
-                <button onClick={()=>{
+                <Button variant="contained" onClick={()=>{
                         const dirs = this.rawTileContainer.getAllConnectableTilesId(this.state.findConnectableId, MetricDirection2D.UP);
                         for (const dir of dirs) {
                             console.log(`Id: ${dir.id}, direction: ${dir.direction}`);
                         }
                     }} 
-                    value="">Find all connectables</button>
+                    value="">Find all connectables</Button>
             </div>  
         );
     }
